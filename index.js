25ad8aaaf8dd56643a6b5f3a9c14722310ff1a58
tarie100 <125449345+tarie100@users.noreply.github.com>
2023-11-16 22:19:58 +0200

    added forecast API

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function refreshWeather(response){
     windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     tempelement.innerHTML= Math.round(temperature);
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
+    getForecast(response.data.city);
 }
 function formatDate(date){
     let minutes = date.getMinutes();
@@ -37,9 +38,49 @@ function formatDate(date){
 
     return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+   return days[date.getDay()]; 
+}
+function getForecast(city) {
+    let apikey = "4ec9b247b78do0486tc7cae2df03aa2e";
+    let apiurl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}&units=metric`;
+    axios(apiurl).then(displayForecast);
+}
+
+function displayForecast(response){
+    let forehtml = "";
+
+    response.data.daily.forEach(function (day, index){
+        if (index < 5){
+            forehtml =
+            forehtml +
+            `
+            <div class="weather-forecast" id="forecast">
+           <div class="row">
+            <div class="col-2">
+                <div class="fore-date">${formatDay(day.time)}</div>
+                <img src="${day.condition.icon_url}" class="fore-icon" />
+                <div class="fore-temp">
+                    <span class="temp-max">${Math.round(day.temperature.maximum)}° </span>
+                    <span class="temp-min">${Math.round(day.temperature.minimum)}° </span>
+                </div>
+
+            </div>
+            
+           </div>
+        </div>
+        `;
+        }
+    });
+
+let foreElement = document.querySelector("#forecast");
+foreElement.innerHTML = forehtml;
+}
 function searchCity(city){
     let apiKey = "4ec9b247b78do0486tc7cae2df03aa2e";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
 }
 function handlesearch(event){
